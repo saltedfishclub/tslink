cb6ebb03e6ed1587b73b630148dda1b5a1a74c72
nc <admin@nullcat.cn>
2026-06-10 01:25:59 +0800

    chore(forwarder): adjust log level

--- core/tcp.go
+++ core/tcp.go
@@ -82,7 +82,7 @@ func runTCPConnector(ctx context.Context, srv *tsnet.Server, rule ConnectRule, l
 		logger.Error("failed to listen locally", "error", err)
 		return
 	}
-	logger.Info("listening", slog.String("on", addr))
+	logger.Debug("listening", slog.String("on", addr))
 
 	go func() {
 		<-ctx.Done()
